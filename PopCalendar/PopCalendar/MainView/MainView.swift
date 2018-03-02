81c50835605eaf4a5fe9279db420f914c13cabb7
RyuYama <r_yamaguchi@dscnet.co.jp>
2018-03-02 16:59:44 +0900

    1st Commit

---
+++ PopCalendar/PopCalendar/MainView/MainView.swift
@@ -0,0 +1,27 @@
+//
+//  MainView.swift
+//  PopCalendar
+//
+//  Created by dsc on 2018/03/02.
+//  Copyright © 2018年 RyuYama. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIViewController
+{
+    
+    /* load
+     */
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    /* memory
+     */
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+}
